cb1a0766ac4c1037de4419805ce297a1a519524b
Artur Spengler <letztes@gmail.com>
2012-07-07 16:37:18 +0200

    Add logging.

--- juggler.pl
+++ juggler.pl
@@ -30,10 +30,19 @@ use URI::Escape;
 
 my $DEVICE = 'wlan0';
 
-# a_star
-my %VISITED;
-
+# Coordinates are keys in two dimensional hash
 my %LETTERS;
+
+# Load initially the dictionary file
+my %DICTIONARY;
+
+# Don't print multiple times the same word, even if found on different
+# locations
+my %FOUND_WORDS;
+
+# File handles
+my $LOGFILE;
+my $PACKET_LOGFILE;
 
 ########################################################################
 # Subroutines
@@ -69,22 +78,20 @@ sub get_letters_hash {
         },
     );
     
-    say " ( $letters{0}{0} ) ( $letters{0}{1} ) ( $letters{0}{2} ) ( $letters{0}{3} ) ";
-    say " ( $letters{1}{0} ) ( $letters{1}{1} ) ( $letters{1}{2} ) ( $letters{1}{3} ) ";
-    say " ( $letters{2}{0} ) ( $letters{2}{1} ) ( $letters{2}{2} ) ( $letters{2}{3} ) ";
-    say " ( $letters{3}{0} ) ( $letters{3}{1} ) ( $letters{3}{2} ) ( $letters{3}{3} ) ";
+    say $LOGFILE  localtime() . ' ' . " ( $letters{0}{0} ) ( $letters{0}{1} ) ( $letters{0}{2} ) ( $letters{0}{3} ) ";
+    say $LOGFILE  localtime() . ' ' . " ( $letters{1}{0} ) ( $letters{1}{1} ) ( $letters{1}{2} ) ( $letters{1}{3} ) ";
+    say $LOGFILE  localtime() . ' ' . " ( $letters{2}{0} ) ( $letters{2}{1} ) ( $letters{2}{2} ) ( $letters{2}{3} ) ";
+    say $LOGFILE  localtime() . ' ' . " ( $letters{3}{0} ) ( $letters{3}{1} ) ( $letters{3}{2} ) ( $letters{3}{3} ) ";
     
     return %letters;
 }
 
 sub find_words {
+    say '------------------------------------------------------';
     
     foreach my $dim_1 (sort keys %LETTERS) {
         
         foreach my $dim_2 (sort keys %{$LETTERS{$dim_1}}) {
-            # %VISITED holds coordinates that were checked for current
-            # staring coordinate
-            %VISITED = ();
             a_star({
                 'dim_1' => $dim_1,
                 'dim_2' => $dim_2,
@@ -93,17 +100,24 @@ sub find_words {
             });
         }
     }
+    
     return;
 }
 
 sub a_star {
     my ($args) = @_;
-    say $args->{'dim_1'} . ', ' . $args->{'dim_2'};
+    #say $args->{'dim_1'} . ', ' . $args->{'dim_2'};
     
     # only words longer than 3 letters are accepted
     if (length($args->{'word'}) > 2) {
         # lookup in dictionary and say word only if exists
-        say $args->{'word'};
+        if ($DICTIONARY{ $args->{'word'} }) {
+            if (not $FOUND_WORDS{ $args->{'word'} }) {
+                say $LOGFILE localtime() . ' ' . $args->{'word'} or die $!;
+                say $args->{'word'};
+                $FOUND_WORDS{ $args->{'word'} } = 1;
+            }
+        }
     }
         
     my @neighbour_coordinates = get_unvisited_neighbours({
@@ -113,8 +127,8 @@ sub a_star {
     });
     return if (not @neighbour_coordinates);
     
-    sleep 2;
     foreach my $neighbour_coordinates (@neighbour_coordinates) {
+        say $LOGFILE localtime() . ' ' . $neighbour_coordinates->[0] . ', ' . $neighbour_coordinates->[1] . ', ' . $args->{'word'} . $LETTERS{ $neighbour_coordinates->[0] }{ $neighbour_coordinates->[1] } or die $!;
         $args->{'visited_href'} -> { $neighbour_coordinates->[0] }{ $neighbour_coordinates->[1] } = 1;
         a_star({
             'dim_1'   => $neighbour_coordinates->[0],
@@ -151,23 +165,45 @@ sub get_unvisited_neighbours {
     return @unvisited_neighbours;
 }
 
+sub fill_dictionary {
+    say 'open dictionary file...';
+    #open(my $WORDSTREAM, qq[aspell -l de dump master | aspell -l de expand | tr ' ' '\n'|]);
+    open(my $WORDSTREAM, 'dictionary.txt');
+    while (my $word = <$WORDSTREAM>) {
+        $word =~ s/ä/ae/g;
+        $word =~ s/ö/oe/g;
+        $word =~ s/ü/ue/g;
+        $word =~ s/ß/ss/g;
+        next if length($word) < 3;
+        next if length($word) > 16;
+        chomp $word;
+        $DICTIONARY{uc($word)} = 1;
+    }
+    say 'done reading ' . (scalar (keys %DICTIONARY)) . ' words.';
+    return;
+}
+
 sub main {
-    #here
-    #open(NETTRAFFIC, "sudo tcpdump -i $DEVICE -Aln host 193.254.186.182 or host 193.254.186.183 or host 194.112.167.227 or host 213.95.79.43 -s 0|") or die $!;
+    open($LOGFILE, '>logfile.txt') or die $!;
+    open($PACKET_LOGFILE, '>packet_logfile.txt') or die $!;
+    open(my $NETTRAFFIC, "sudo tcpdump -i $DEVICE -Aln host 193.254.186.182 or host 193.254.186.183 or host 194.112.167.227 or host 213.95.79.43 -s 0|") or die $!;
+    fill_dictionary();
     
-    #here
-    #while (my $packet = <NETTRAFFIC>) {
-    while (1) {
-        #say $packet;
-        #here
-        #if (my @letters = $packet =~ m/Juggle.+#JuggleBoT.+#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])$/) {
-        if (my @letters = 'gJuggle2#JuggleBoTina#27#0#i#2#180#E#A#C#L#S#O#T#L#N#E#E#R#A#C#S#B' =~ m/Juggle.+#JuggleBoT.+#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])$/) {
+    while (my $packet = <$NETTRAFFIC>) {
+        exit if $packet =~ m/Abschied/;
+        say $PACKET_LOGFILE localtime() . ' ' .  $packet or die $!;
+        if (my @letters = $packet =~ m/([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])$/) {
+        #if (my @letters = 'gJuggle2#JuggleBoTina#27#0#i#2#180#E#A#C#L#S#O#T#L#N#E#E#R#A#C#S#B' =~ m/Juggle.+#JuggleBoT.+#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])#([A-Z])$/) {
             #say $packet;
-            say Dumper(@letters);
+            #say Dumper(@letters);
             %LETTERS = get_letters_hash(@letters);
-            find_words(); # from global %LETTERS and say them words immediately
+            find_words();
         }
     }
+    close($NETTRAFFIC);
+    close($PACKET_LOGFILE);
+    close($LOGFILE);
+    exit;
 }
 
 
